c96ce1a551f118087942589080cece63585d2a45
Sergi Virgili <svirgilif@gmail.com>
2020-03-07 16:19:16 +0100

    Add Student Dao Interface

--- DataAccess/IStudentDao.cs
+++ DataAccess/IStudentDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace DataAccess
 {
     interface IStudentDao
     {
+        Student Add(StudentDao student);
+        Student Delete(StudentDao student);
+        Student GetAll();
+        Student Update(StudentDao student);
+        Student GetById(int id);
     }
 }
