e922fb3521348eee940bc0a30a9632d350173bc6
Sergi Virgili <svirgilif@gmail.com>
2020-03-07 16:25:24 +0100

    Add Student Dao Class

--- DataAccess/StudentDao.cs
+++ DataAccess/StudentDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class Class1
+    public class StudentDao : IStudentDao
     {
+        public Student Add(StudentDao student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Delete(StudentDao student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student Update(StudentDao student)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
