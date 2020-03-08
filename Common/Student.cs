fc867ccc1aedca3c82555a35ab278994b5251a1b
Sergi Virgili <svirgilif@gmail.com>
2020-03-08 17:28:31 +0100

    Create Student Model in Common Layer. Close #15

--- Common/Student.cs
+++ Common/Student.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Common
 {
     public class Student
     {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime AgeOfBirth { get; set; }
     }
 }
