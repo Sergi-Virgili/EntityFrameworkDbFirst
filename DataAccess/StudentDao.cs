567c8ad4c6def8ff5da7386270d977129c442051
Sergi Virgili <svirgilif@gmail.com>
2020-03-09 20:11:05 +0100

    Add Logs and Exceptions to StudentDao Add Method

--- DataAccess/StudentDao.cs
+++ DataAccess/StudentDao.cs
@@ -2,36 +2,48 @@
 using DataAccess.SQLDataBase;
 using log4net;
 using System;
+
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 
 namespace DataAccess
 {
     public class StudentDao : IStudentDao
     {
+        private Student addedStudent;
         private static readonly ILog log = LogManager.GetLogger(typeof(StudentDao));
         public Student Add(Student student)
         {
-            log.Info(student.Name + " - Start ADD");
-            TableStudent addedStudent;
+            
             try
             {
                 using (VuelingDbContext vueling = new VuelingDbContext())
                 {
-                    var studentTable = new StudentMap().ToStudentTable(student);
-                    addedStudent = vueling.TableStudents.Add(studentTable);
+                    var studentTable =  StudentMap.ToStudentTable(student);
+                    var TableAddedStudent = vueling.TableStudents.Add(studentTable);
+                    addedStudent = StudentMap.ToStudent(TableAddedStudent);
                     vueling.SaveChanges();     
                 }
             }
-            catch (Exception ex)
+            catch (DbUpdateException ex)
             {
-                log.Error("Exception Message", ex);
+                log.Error("DataBase Update Exception With Data" + LogMessage.Student(addedStudent), ex);
                 throw;
             }
-            log.Info("Student saved");
-            return new StudentMap().ToStudent(addedStudent);
-
+            catch (DbEntityValidationException ex)
+            {
+                log.Error("Entity Validation Exception With Data" + LogMessage.Student(addedStudent), ex);
+                throw;
+            }
+            catch (NotSupportedException ex)
+            {
+                log.Error("Action Not Supported With Data" + LogMessage.Student(addedStudent), ex);
+                throw;
+            }
+            log.Info("ADDED: " + LogMessage.Student(addedStudent));
+            return addedStudent;
         }
 
         public bool Delete(Student student)
@@ -51,7 +63,7 @@ namespace DataAccess
             {
                 var studentsTable = vueling.TableStudents.ToList();
                 var students = new List<Student>();
-                studentsTable.ForEach(studentTable => students.Add(new StudentMap().ToStudent(studentTable)));
+                studentsTable.ForEach(studentTable => students.Add(StudentMap.ToStudent(studentTable)));
                 return students;
             }
         }
@@ -61,7 +73,7 @@ namespace DataAccess
             using (VuelingDbContext vueling = new VuelingDbContext())
             {
                 var findedStudent = vueling.TableStudents.Where(s => s.StudentId == id).FirstOrDefault();
-                return new StudentMap().ToStudent(findedStudent);
+                return StudentMap.ToStudent(findedStudent);
             }
         }
 
@@ -70,7 +82,7 @@ namespace DataAccess
             using (VuelingDbContext vueling = new VuelingDbContext())
             {
                 var studentUpdate = vueling.TableStudents.Where(s => s.StudentId == student.StudentId).FirstOrDefault();
-                var studentUpdated = new StudentMap().ToStudentTable(student);
+                var studentUpdated = StudentMap.ToStudentTable(student);
                 vueling.Entry(studentUpdate).CurrentValues.SetValues(studentUpdated);
                 vueling.SaveChanges();
                 return student;
